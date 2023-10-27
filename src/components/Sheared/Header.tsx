e65a207b17bbc6f4cf53d1dbfc6696a31049238c
mr7aali <mr7aali@gmail.com>
2023-10-27 15:11:40 +0600

    Ha

--- src/components/Sheared/Header.tsx
+++ src/components/Sheared/Header.tsx
@@ -5,6 +5,7 @@ import {
   GithubOutlined,
   MenuOutlined,
 } from "@ant-design/icons";
+import Link from "next/link";
 
 const Header = () => {
   const NavItem = ["about", "book", "portfolio", "blog", "contact"];
@@ -17,14 +18,33 @@ const Header = () => {
   return (
     <header className="bg-[#111]">
       <div className="max-w-[1040px] mx-auto flex py-6 items-center">
-        <h1 className="text-[#fff] flex-1 mx-5 text-[40px] font-bold ">Aali</h1>
-        <div className="flex items-center">
+
+
+
+      
+       <Link
+          href={"/"}
+          className="text-[#fff] text-[25px] flex-1 mx-5 md:text-[40px] font-bold no-underline"
+        >
+          Aali
+        </Link>
+        
+        <div  className="md:hidden mr-3">
+          <span className="text-[#fff]  text-[25px] md:text-[30px]  cursor-pointer hover:opacity-50 delay-150 transition-all">
+            <MenuOutlined />
+          </span>
+        </div>
+       
+
+
+        <div className="hidden md:flex items-center">
           <ul className="list-none flex">
             {NavItem.map((Item) => (
               <li key={Item} className=" cursor-pointer ">
                 <span
-                //  style={{border:'1px solid red'}} 
-                 className="py-4 px-2 xl:p-4 hover:opacity-50 text-[#fff] text-[18px] transition ease-linear delay-150 font-thin">
+                  //  style={{border:'1px solid red'}}
+                  className="py-4 px-2 xl:p-4 hover:opacity-50 text-[#fff] text-[18px] transition ease-linear delay-150 font-thin"
+                >
                   {Item}
                 </span>
               </li>
@@ -42,12 +62,12 @@ const Header = () => {
               </span>
             ))}
           </div>
-          <div className="hidden">
-            <span className="text-[#fff] text-[30px] p-3 cursor-pointer hover:opacity-50 delay-150 transition-all">
-              <MenuOutlined />
-            </span>
-          </div>
         </div>
+
+       
+
+
+
       </div>
     </header>
   );
