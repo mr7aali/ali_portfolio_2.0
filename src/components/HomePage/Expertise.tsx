32a16b092c4d95cd86fa518c48bef56e013bfb74
mr7aali <mr7aali@gmail.com>
2023-10-28 01:48:28 +0600

    responsive done for expertise

--- src/components/HomePage/Expertise.tsx
+++ src/components/HomePage/Expertise.tsx
@@ -24,7 +24,7 @@ const Expertise = () => {
   ];
   return (
     <div className="mt-14 max-w-[1220px] mx-auto">
-      <h1 className="text-center text-[50px] font-bold font-serif">
+      <h1 className="text-center text-[50px] font-bold font-serif ">
         My Expertise
       </h1>
       <div
@@ -34,29 +34,32 @@ const Expertise = () => {
           borderLeft: "5px solid #111",
         //   border:'1px solid red'
         }}
-        className=" grid grid-cols-3 my-5"
+        className=" grid grid-cols-1 md:grid-cols-3 my-5 mx-2"
       >
         {Items.map((item, index) => (
           <div
             key={item.title}
             style={{
               borderRight: "5px solid #111",
+              // border:'1px solid red'
             }}
-            className="py-12 px-2"
+            
+
+            className="py-3 lg:py-8 px-2"
           >
             <div  className="flex items-center">
-              <span className="text-[50px] px-5 text">{<item.Icon className="" />}</span>
-              <h3 className="text-[30px]">{item.title}</h3>
+              <span  className="text-[50px] px-2 lg:px-5 text">{<item.Icon className="" />}</span>
+              <h3 className="text-[25px] lg:text-[30px]">{item.title}</h3>
             </div>
 
 
 
             <div className="ml-2">
-              <p className="font-bold opacity-50 py-2">{"<h3>"}</p>
+              <p className="font-bold opacity-50 py-2 font-serif">{"<h3>"}</p>
               <div style={{borderLeft:'3px solid #111'}} className="ml-4">
-              <span className="text-[20px] block p-4">{item.description}</span>
+              <span className=" lg:text-[18px] block p-4 font-mono font-extrabold">{item.description}</span>
               </div>
-              <p className="font-bold opacity-50 py-2">{"</h3>"}</p>
+              <p className="font-bold opacity-50 py-2 font-serif">{"</h3>"}</p>
             </div>
 
 
