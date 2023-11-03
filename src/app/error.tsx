a8adbeee45baa5a506d1569e699894c5f59cfdc3
mr7aali <mr7aali@gmail.com>
2023-11-04 00:11:49 +0600

    dynamic import

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>My error Page</h1>
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-red-600 uppercase">My error Page</h1>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,23 @@
-import AboutMe from "@/components/HomePage/AboutMe";
-import ContactWithMe from "@/components/HomePage/Contact";
-import Expertise from "@/components/HomePage/Expertise";
-import HeroSection from "@/components/HomePage/HeroSection";
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Sheared/Footer";
-import Header from "@/components/Sheared/Header";
+import dynamic from "next/dynamic";
+const AboutMe = dynamic(() => import("@/components/HomePage/AboutMe"));
+const ContactWithMe = dynamic(() => import("@/components/HomePage/Contact"));
+const Expertise = dynamic(() => import("@/components/HomePage/Expertise"));
+const HeroSection = dynamic(() => import("@/components/HomePage/HeroSection"));
+const Footer = dynamic(() => import("@/components/Sheared/Footer"));
+const Header = dynamic(() => import("@/components/Sheared/Header"));
 
-// import { motion } from "framer-motion";
 
 const HomePage = () => {
   return (
     <div>
-      
       <Header />
-      <HeroSection/>
-      <Expertise/>
-      <AboutMe/>
-      <ContactWithMe/>
-      <Footer/>
+      <HeroSection />
+      <Expertise />
+      <AboutMe />
+      <ContactWithMe />
+      <Footer />
 
       {/* <div className="mb-[100px]"></div> */}
-
     </div>
   );
 };
